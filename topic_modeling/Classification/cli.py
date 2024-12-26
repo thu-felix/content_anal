a6b15574e87c1fe94e90b5ebc33d1fc31329f051
C.H.AL <74540801+KohakuKitsune@users.noreply.github.com>
2024-12-26 09:36:20 +0800

    Update cli.py

--- topic_modeling/Classification/cli.py
+++ topic_modeling/Classification/cli.py
@@ -2,11 +2,14 @@
 
 import os
 import sys
-sys.path.append("/content/content_anal/topic_modeling/Classification")
+sys.path.append(".")
 
 import argparse
+
 from openprompt.trainer import ClassificationRunner, GenerationRunner
+from openprompt.lm_bff_trainer import LMBFFClassificationRunner
 from openprompt.protoverb_trainer import ProtoVerbClassificationRunner
+from re import template
 from openprompt.pipeline_base import PromptForClassification, PromptForGeneration
 from openprompt.utils.reproduciblity import set_seed
 from openprompt import PromptDataLoader
@@ -15,19 +18,20 @@ from openprompt.data_utils import FewShotSampler
 from openprompt.utils.logging import config_experiment_dir, init_logger, logger
 from openprompt.config import get_config, save_config_to_yaml
 from openprompt.plms import load_plm_from_config
+# from openprompt.data_utils import load_dataset
 from custom_dataset_loader import load_dataset
 from openprompt.utils.cuda import model_to_device
 
-def build_dataloader(dataset, template, tokenizer, tokenizer_wrapper_class, config, split):
+def build_dataloader(dataset, template, tokenizer,tokenizer_wrapper_class, config, split):
     dataloader = PromptDataLoader(
-        dataset=dataset,
-        template=template,
-        tokenizer=tokenizer,
+        dataset = dataset,
+        template = template,
+        tokenizer = tokenizer,
         tokenizer_wrapper_class=tokenizer_wrapper_class,
-        batch_size=config[split].batch_size,
-        shuffle=config[split].shuffle_data,
-        teacher_forcing=config[split].teacher_forcing if hasattr(config[split], 'teacher_forcing') else None,
-        predict_eos_token=True if config.task == "generation" else False,
+        batch_size = config[split].batch_size,
+        shuffle = config[split].shuffle_data,
+        teacher_forcing = config[split].teacher_forcing if hasattr(config[split],'teacher_forcing') else None,
+        predict_eos_token = True if config.task == "generation" else False,
         **config.dataloader
     )
     return dataloader
@@ -46,8 +50,9 @@ def main():
         # save config to the logger directory
         save_config_to_yaml(config)
 
+
     # load dataset. The valid_dataset can be None
-    train_dataset, valid_dataset, test_dataset, Processor = load_dataset(config, test=args.test is not None or config.learning_setting == 'zero_shot')
+    train_dataset, valid_dataset, test_dataset, Processor = load_dataset(config, test = args.test is not None or config.learning_setting == 'zero_shot')
 
     # main
     if config.learning_setting == 'full':
@@ -55,11 +60,11 @@ def main():
             EXP_PATH,
             config,
             Processor,
-            resume=args.resume,
-            test=args.test,
-            train_dataset=train_dataset,
-            valid_dataset=valid_dataset,
-            test_dataset=test_dataset,
+            resume = args.resume,
+            test = args.test,
+            train_dataset = train_dataset,
+            valid_dataset = valid_dataset,
+            test_dataset = test_dataset,
         )
     elif config.learning_setting == 'few_shot':
         if config.few_shot.few_shot_sampling is None:
@@ -69,32 +74,32 @@ def main():
         for seed in seeds:
             if not args.test:
                 sampler = FewShotSampler(
-                    num_examples_per_label=config.sampling_from_train.num_examples_per_label,
-                    also_sample_dev=config.sampling_from_train.also_sample_dev,
-                    num_examples_per_label_dev=config.sampling_from_train.num_examples_per_label_dev
+                    num_examples_per_label = config.sampling_from_train.num_examples_per_label,
+                    also_sample_dev = config.sampling_from_train.also_sample_dev,
+                    num_examples_per_label_dev = config.sampling_from_train.num_examples_per_label_dev
                 )
                 train_sampled_dataset, valid_sampled_dataset = sampler(
-                    train_dataset=train_dataset,
-                    valid_dataset=valid_dataset,
-                    seed=seed
+                    train_dataset = train_dataset,
+                    valid_dataset = valid_dataset,
+                    seed = seed
                 )
                 result = trainer(
                     os.path.join(EXP_PATH, f"seed-{seed}"),
                     config,
                     Processor,
-                    resume=args.resume,
-                    test=args.test,
-                    train_dataset=train_sampled_dataset,
-                    valid_dataset=valid_sampled_dataset,
-                    test_dataset=test_dataset,
+                    resume = args.resume,
+                    test = args.test,
+                    train_dataset = train_sampled_dataset,
+                    valid_dataset = valid_sampled_dataset,
+                    test_dataset = test_dataset,
                 )
             else:
                 result = trainer(
                     os.path.join(EXP_PATH, f"seed-{seed}"),
                     config,
                     Processor,
-                    test=args.test,
-                    test_dataset=test_dataset,
+                    test = args.test,
+                    test_dataset = test_dataset,
                 )
             res += result
         res /= len(seeds)
@@ -103,13 +108,13 @@ def main():
             EXP_PATH,
             config,
             Processor,
-            zero=True,
-            train_dataset=train_dataset,
-            valid_dataset=valid_dataset,
-            test_dataset=test_dataset,
+            zero = True,
+            train_dataset = train_dataset,
+            valid_dataset = valid_dataset,
+            test_dataset = test_dataset,
         )
 
-def trainer(EXP_PATH, config, Processor, train_dataset=None, valid_dataset=None, test_dataset=None, resume=None, test=None, zero=False):
+def trainer(EXP_PATH, config, Processor, train_dataset = None, valid_dataset = None, test_dataset = None, resume = None, test = None, zero = False):
     if not os.path.exists(EXP_PATH):
         os.mkdir(EXP_PATH)
     config.logging.path = EXP_PATH
@@ -119,18 +124,17 @@ def trainer(EXP_PATH, config, Processor, train_dataset=None, valid_dataset=None,
     # load the pretrained models, its model, tokenizer, and config.
     plm_model, plm_tokenizer, plm_config, plm_wrapper_class = load_plm_from_config(config)
 
-    # define template and verbalizer for topic modeling
+    # define template and verbalizer
     if config.task == "classification":
-        # Load soft prompt template and verbalizer for topic modeling task
+        # define prompt
         template = load_template(config=config, model=plm_model, tokenizer=plm_tokenizer, plm_config=plm_config)
         verbalizer = load_verbalizer(config=config, model=plm_model, tokenizer=plm_tokenizer, plm_config=plm_config, classes=Processor.labels)
-        
         # load prompt’s pipeline model
-        prompt_model = PromptForClassification(plm_model, template, verbalizer, freeze_plm=config.plm.optimize.freeze_para)
+        prompt_model = PromptForClassification(plm_model, template, verbalizer, freeze_plm = config.plm.optimize.freeze_para)
 
     elif config.task == "generation":
         template = load_template(config=config, model=plm_model, tokenizer=plm_tokenizer, plm_config=plm_config)
-        prompt_model = PromptForGeneration(plm_model, template, freeze_plm=config.plm.optimize.freeze_para, gen_config=config.generation)
+        prompt_model = PromptForGeneration(plm_model, template, freeze_plm = config.plm.optimize.freeze_para, gen_config = config.generation)
     else:
         raise NotImplementedError(f"config.task {config.task} is not implemented yet. Only classification and generation are supported.")
 
@@ -141,37 +145,48 @@ def trainer(EXP_PATH, config, Processor, train_dataset=None, valid_dataset=None,
 
     if config.task == "classification":
         if config.classification.auto_t or config.classification.auto_v:
-            runner = ProtoVerbClassificationRunner(model=prompt_model,
-                                                   train_dataloader=train_dataloader,
-                                                   valid_dataloader=valid_dataloader,
-                                                   test_dataloader=test_dataloader,
-                                                   id2label=Processor.id2label,
-                                                   config=config)
+            runner = LMBFFClassificationRunner(train_dataset = train_dataset,
+                                                valid_dataset = valid_dataset,
+                                                test_dataset = test_dataset,
+                                                template=template,
+                                                verbalizer=verbalizer,
+                                                config = config
+                                                )
+        elif config.verbalizer == "proto_verbalizer":
+            runner = ProtoVerbClassificationRunner(model = prompt_model,
+                                    train_dataloader = train_dataloader,
+                                    valid_dataloader = valid_dataloader,
+                                    test_dataloader = test_dataloader,
+                                    id2label = Processor.id2label,
+                                    config = config
+            )
         else:
-            runner = ClassificationRunner(model=prompt_model,
-                                         train_dataloader=train_dataloader,
-                                         valid_dataloader=valid_dataloader,
-                                         test_dataloader=test_dataloader,
-                                         id2label=Processor.id2label,
-                                         config=config)
+            runner = ClassificationRunner(model = prompt_model,
+                                    train_dataloader = train_dataloader,
+                                    valid_dataloader = valid_dataloader,
+                                    test_dataloader = test_dataloader,
+                                    id2label = Processor.id2label,
+                                    config = config
+            )
     elif config.task == "generation":
         runner = GenerationRunner(
-            model=prompt_model,
-            train_dataloader=train_dataloader,
-            valid_dataloader=valid_dataloader,
-            test_dataloader=test_dataloader,
-            config=config
+            model = prompt_model,
+            train_dataloader = train_dataloader,
+            valid_dataloader = valid_dataloader,
+            test_dataloader = test_dataloader,
+            config = config
         )
 
     if zero:
         res = runner.test()
     elif test:
-        res = runner.test(ckpt='best')
+        res = runner.test(ckpt = 'best')
     elif resume:
-        res = runner.run(ckpt='last')
+        res = runner.run(ckpt = 'last')
     else:
         res = runner.run()
     return res
+
 
 if __name__ == "__main__":
     main()
